69c24fea32cc4629f5f665e55365746bb21486d4
Mangal <lazehang@gmail.com>
2019-11-13 22:07:46 +0800

    gtm

--- layouts/default.js
+++ layouts/default.js
@@ -7,5 +7,13 @@ export default ({ children }) => (
     <Nav/>
     { children }
     <Footer />
+    <script dangerouslySetInnerHTML={{__html: `(function(d, id) {
+        if (d.getElementById(id)) return;
+        var sc, node = d.getElementsByTagName('script')[0];
+        sc = d.createElement('script'); sc.id = id;
+        sc.src = 'https://sdk.getsitekit.com/static/js/app.js#apiKey=592d3dec20e076287d2e91cdd6a16be3';
+        node.parentNode.insertBefore(sc, node);
+      }(document, 'sitekit-sdk'));`}}>
+    </script>
   </div>
 )
